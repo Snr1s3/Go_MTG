237fba924c5e926fe85a5bb16a46f27395cc1822
Alba Segura <m1ss.sunrse@gmail.com>
2026-05-06 12:55:48 +0200

    update becasue file too large in memory

--- fakeAPI/main.py
+++ fakeAPI/main.py
@@ -4,15 +4,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-with open('file.json', 'r') as f:
-    data = json.load(f)
+data_file = '/home/ec2-user/Go_MTG/fakeAPI/data/all-cards-20260506092337.json'
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
-
 @app.get("/cards/{card_id}")
 def get_card(card_id: str):
-    return data.get(card_id,{"ERROR": "CARD NOT FOUND"})
+    try:
+        with open(data_file, 'r') as f:
+            for line in f:
+                card = json.loads(line)
+                if card.get('id') == card_id or card.get('name') == card_id:
+                    return card
+        return {"ERROR": "CARD NOT FOUND"}
+    except Exception as e:
+        return {"ERROR": str(e)}
